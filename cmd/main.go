8fcbcf038f080616529e30f729f2b5ad5b9c6ae8
Dennis Buijs <dennis.buijs@cm.com>
2023-10-07 19:20:37 +0200

    Add support for multiple rooms

--- cmd/main.go
+++ cmd/main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/r3labs/sse/v2"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"strings"
-
-	"github.com/r3labs/sse/v2"
 )
 
-type Answer struct {
-	Answer string
+type Room struct {
+	Name       string
+	Slug       string
+	Image      string
+	Scoreboard Scoreboard
 }
 
 type Player struct {
@@ -30,108 +33,49 @@ type Scoreboard struct {
 }
 
 var SseServer *sse.Server
-var scoreboard Scoreboard
+var rooms []Room
 
 func main() {
 	SseServer = sse.New()
 	SseServer.CreateStream("scoreboard")
 
-	var players []Player
-	scoreboard = Scoreboard{}
+	rooms = []Room{
+		{
+			Name:  "Classic Rock",
+			Slug:  "classic-rock",
+			Image: "../web/images/guitar.svg",
+			Scoreboard: Scoreboard{
+				Scores: []Score{},
+			},
+		},
+		{
+			Name:  "Pop Hits",
+			Slug:  "pop-hits",
+			Image: "../web/images/pop.svg",
+			Scoreboard: Scoreboard{
+				Scores: []Score{},
+			},
+		},
+	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.ParseFiles("../web/templates/base.html", "../web/templates/home.html")
+	r := mux.NewRouter()
 
-		if err != nil {
-			fmt.Println(err)
-		}
+	r.HandleFunc("/", homeRouteHandler)
+	r.HandleFunc("/room/{slug}", gameRouteHandler)
+	r.HandleFunc("/room/{slug}/join", registerPlayerHandler)
 
-		err = t.Execute(w, nil)
-		if err != nil {
-			fmt.Println(err)
-		}
-	})
+	r.HandleFunc("/events", SseServer.ServeHTTP)
+	r.HandleFunc("/web/{dir}/{filepath:.*}", staticHandler)
 
-	http.HandleFunc("/quiz", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.ParseFiles("../web/templates/base.html", "../web/templates/game.html")
+	http.Handle("/", r)
 
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		err = t.Execute(w, nil)
-		if err != nil {
-			fmt.Println(err)
-		}
-	})
-
-	http.HandleFunc("/player/register", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		err := r.ParseForm()
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		var player Player
-		player.Name = r.FormValue("name")
-
-		players = append(players, player)
-
-		var score Score
-		score.Player = player
-		score.Score = 0
-
-		scoreboard.Scores = append(scoreboard.Scores, score)
-
-		emitScoreboardUpdate()
-	})
-
-	http.HandleFunc("/answer", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		// r.ParseForm()
-
-		// var answer Answer
-		// answer.Answer = r.FormValue("answer")
-	})
-
-	http.HandleFunc("/web/", staticHandler)
-
-	http.HandleFunc("/events", SseServer.ServeHTTP)
-
-	err := http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", r)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func staticHandler(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	if strings.HasSuffix(path, "js") {
-		w.Header().Set("Content-Type", "text/javascript")
-	} else if strings.HasSuffix(path, "css") {
-		w.Header().Set("Content-Type", "text/css")
-	} else {
-		w.Header().Set("Content-type", "image/svg+xml")
-	}
-	data, err := os.ReadFile("../" + path[1:])
-	if err != nil {
-		fmt.Print(err)
-	}
-	_, err = w.Write(data)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func emitScoreboardUpdate() {
+func emitScoreboardUpdate(scoreboard Scoreboard) {
 	var templateBuffer bytes.Buffer
 	t, _ := template.ParseFiles("../web/templates/scoreboard.html")
 	err := t.Execute(&templateBuffer, scoreboard)
@@ -142,4 +86,97 @@ func emitScoreboardUpdate() {
 	SseServer.Publish("scoreboard", &sse.Event{
 		Data: bytes.ReplaceAll(templateBuffer.Bytes(), []byte("\n"), []byte("")),
 	})
+}
+
+func registerPlayerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	routeParams := mux.Vars(r)
+	slug := routeParams["slug"]
+	var room = findRoom(slug)
+
+	if room == nil {
+		http.Error(w, "Room not found", http.StatusNotFound)
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var player Player
+	player.Name = r.FormValue("name")
+
+	var score Score
+	score.Player = player
+	score.Score = 0
+
+	room.Scoreboard.Scores = append(room.Scoreboard.Scores, score)
+
+	emitScoreboardUpdate(room.Scoreboard)
+}
+
+func findRoom(slug string) *Room {
+	for i := range rooms {
+		if rooms[i].Slug == slug {
+			return &rooms[i]
+		}
+	}
+
+	return nil
+}
+
+func homeRouteHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("../web/templates/base.html", "../web/templates/home.html")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = t.Execute(w, rooms)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func gameRouteHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("../web/templates/base.html", "../web/templates/game.html")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	routeParams := mux.Vars(r)
+	slug := routeParams["slug"]
+	var room = findRoom(slug)
+
+	err = t.Execute(w, room)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+
+	if strings.HasSuffix(path, "js") {
+		w.Header().Set("Content-Type", "text/javascript")
+	} else if strings.HasSuffix(path, "css") {
+		w.Header().Set("Content-Type", "text/css")
+	} else {
+		w.Header().Set("Content-type", "image/svg+xml")
+	}
+
+	data, err := os.ReadFile("../" + path[1:])
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	_, err = w.Write(data)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module musicquiz
 go 1.21.1
 
 require (
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/r3labs/sse/v2 v2.10.0 // indirect
 	golang.org/x/net v0.16.0 // indirect
 	gopkg.in/cenkalti/backoff.v1 v1.1.0 // indirect
